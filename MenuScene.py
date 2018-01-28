7c0ead8077103248e1054aeaee2ffcb226b6f6e6
Mark Pavlovski <markpavlovski@gmail.com>
2018-01-27 20:19:39 -0800

    added main menu brick selector

--- MenuScene.py
+++ MenuScene.py
@@ -1,6 +1,7 @@
 import pygame
 from Scene import *
 from GameConstants import *
+from Brick import *
 
 class MenuScene(Scene):
 
@@ -14,12 +15,24 @@ class MenuScene(Scene):
         self.__menu_sprite = pygame.image.load(GameConstants.SPRITE_MENU)
         self.__start_sound = True
 
+        self.__brick_current_location = 0
+
     def render(self):
+        game = self.get_game()
         self.get_game().screen.blit(self.__menu_sprite,(50,50))
         super(MenuScene,self).render()
         while self.__start_sound:
             self.get_game().play_sound(GameConstants.SOUND_BREAKOUT)
             self.__start_sound = False
+
+        start_x = 325
+        start_y = 312
+        linebreak = 50
+        brick_locations = [(start_x,start_y),(start_x,start_y + linebreak),(start_x,start_y+2*linebreak)]
+        brick = Brick(brick_locations[self.__brick_current_location], pygame.image.load(GameConstants.SPRITE_BRICK_RED), game)
+        game.screen.blit(Brick.get_sprite(brick),brick.get_position())
+
+
 
 
     def handle_events(self, events):
@@ -36,11 +49,21 @@ class MenuScene(Scene):
             if event.type == pygame.KEYDOWN:
                 if event.key  == pygame.K_ESCAPE:
                     exit()
-                if event.key == pygame.K_1:
-                    self.get_game().play_sound(GameConstants.SOUND_NEW_GAME)
-                    self.get_game().change_scene(GameConstants.PLAYING_SCENE)
-                if event.key == pygame.K_2:
-                    self.get_game().play_sound(GameConstants.SOUND_HIGH_SCORES)
-                    self.get_game().change_scene(GameConstants.HIGHSCORE_SCENE)
-                if event.key == pygame.K_3:
-                    exit()
+                if event.key == pygame.K_SPACE:
+                    if self.__brick_current_location == 0:
+                        self.get_game().play_sound(GameConstants.SOUND_NEW_GAME)
+                        self.get_game().change_scene(GameConstants.PLAYING_SCENE)
+                    elif self.__brick_current_location == 1:
+                        self.get_game().play_sound(GameConstants.SOUND_HIGH_SCORES)
+                        self.get_game().change_scene(GameConstants.HIGHSCORE_SCENE)
+                    elif self.__brick_current_location == 2:
+                        exit()
+
+                if event.key == pygame.K_DOWN or event.key == pygame.K_RIGHT:
+                    self.get_game().play_sound(GameConstants.SOUND_HIT_BRICK)
+                    self.__brick_current_location += 1
+                    self.__brick_current_location %= 3
+                if event.key == pygame.K_UP or event.key == pygame.K_LEFT:
+                    self.get_game().play_sound(GameConstants.SOUND_HIT_BRICK)
+                    self.__brick_current_location -= 1
+                    self.__brick_current_location %= 3
